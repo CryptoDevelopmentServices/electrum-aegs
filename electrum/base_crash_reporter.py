49dccf9e0d3200b737bfd29abb61060f5c18e92c
Loshan T <loshan1212@gmail.com>
2023-08-24 05:40:48 +0100

    ltc: use litecoin constants, derivation paths, msg prefix

--- electrum/base_crash_reporter.py
+++ electrum/base_crash_reporter.py
@@ -71,7 +71,7 @@ class BaseCrashReporter(Logger):
 
     def send_report(self, asyncio_loop, proxy, *, timeout=None) -> CrashReportResponse:
         # FIXME the caller needs to catch generic "Exception", as this method does not have a well-defined API...
-        if constants.net.GENESIS[-4:] not in ["4943", "e26f"] and ".electrum-ltc.org" in BaseCrashReporter.report_server:
+        if constants.net.GENESIS[-4:] not in ["29a0", "bfe2"] and ".electrum-ltc.org" in BaseCrashReporter.report_server:
             # Gah! Some kind of altcoin wants to send us crash reports.
             raise Exception(_("Missing report URL."))
         report = self.get_traceback_info()
