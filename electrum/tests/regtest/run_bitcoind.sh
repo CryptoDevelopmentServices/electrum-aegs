9c55e2a9e6f8a6a4dc1e18096304f142bc0a8a05
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-07-24 17:42:14 +0100

    aegs: fix partial tests

--- electrum/tests/regtest/run_bitcoind.sh
+++ electrum/tests/regtest/run_bitcoind.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export HOME=~
 set -eux pipefail
-mkdir -p ~/.litecoin
-cat > ~/.litecoin/litecoin.conf <<EOF
+mkdir -p ~/.aegisum
+cat > ~/.aegisum/aegisum.conf <<EOF
 regtest=1
 txindex=1
 printtoconsole=1
@@ -16,10 +16,10 @@ fallbackfee=0.0002
 rpcbind=0.0.0.0
 rpcport=19554
 EOF
-rm -rf ~/.litecoin/regtest
-litecoind -regtest &
+rm -rf ~/.aegisum/regtest
+aegisumd -regtest &
 sleep 6
-litecoin-cli createwallet test_wallet
-addr=$(litecoin-cli getnewaddress)
-litecoin-cli generatetoaddress 150 $addr
-tail -f ~/.litecoin/regtest/debug.log
+aegisum-cli createwallet test_wallet
+addr=$(aegisum-cli getnewaddress)
+aegisum-cli generatetoaddress 150 $addr
+tail -f ~/.aegisum/regtest/debug.log
--- electrum/tests/regtest/run_electrumx.sh
+++ electrum/tests/regtest/run_electrumx.sh
@@ -4,4 +4,4 @@ set -eux pipefail
 cd
 rm -rf $HOME/electrumx_db
 mkdir $HOME/electrumx_db
-COST_SOFT_LIMIT=0 COST_HARD_LIMIT=0 COIN=Litecoin SERVICES=tcp://:51001,rpc:// NET=regtest DAEMON_URL=http://doggman:donkey@127.0.0.1:18554 DB_DIRECTORY=$HOME/electrumx_db DAEMON_POLL_INTERVAL_BLOCKS=100 DAEMON_POLL_INTERVAL_MEMPOOL=100 electrumx_server
+COST_SOFT_LIMIT=0 COST_HARD_LIMIT=0 COIN=Aegisum SERVICES=tcp://:51001,rpc:// NET=regtest DAEMON_URL=http://doggman:donkey@127.0.0.1:18554 DB_DIRECTORY=$HOME/electrumx_db DAEMON_POLL_INTERVAL_BLOCKS=100 DAEMON_POLL_INTERVAL_MEMPOOL=100 electrumx_server
